402ba62699253ca5d3162582fc2f7b8b6c60f966
Jeremy German <jeremy.german@gmail.com>
2014-02-09 13:35:11 -0500

    adding leiningen

---
+++ lein/profiles.clj
@@ -0,0 +1,9 @@
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [lein-deploy-deps "0.1.0"]
+                  [lein-clojars "0.9.1"]
+                  [lein-midje "3.0.0"]
+		  [lein-localrepo "0.5.2"]
+		  [joodo/lein-joodo "1.2.0"]]
+	:env {:foo-key "barbaz"}
+	}
+	}

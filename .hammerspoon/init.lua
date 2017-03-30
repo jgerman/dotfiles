9cdfaf7cbe61d4e31f60cf82a27d226258398c01
Jeremy German <jgerman@bayta.home>
2017-03-30 10:38:39 -0400

    Adding hammerspoon config.

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,43 @@
+local hints = require "hs.hints"
+local hotkey = require 'hs.hotkey'
+local window = require 'hs.window'
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+
+      f.x = max.x
+      f.y = max.y
+      f.w = max.w / 2
+      f.h = max.h
+      win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+
+      f.x = max.x + (max.w / 2)
+      f.y = max.y
+      f.w = max.w / 2
+      f.h = max.h
+      win:setFrame(f)
+end)
+
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+
+      f.x = max.x
+      f.y = max.y
+      f.w = max.w
+      f.h = max.h
+      win:setFrame(f)
+end)

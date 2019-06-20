237a04af57f8f19a39a6e3e29986fd3ea41f981b
Jeremy German <jgerman@bayta.home>
2019-06-20 10:32:01 -0400

    Added profiles

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [cider/cider-nrepl "0.21.1" :exclusions [org.clojure/tools.nrepl]]
+                  
+                  [lein-exec "0.3.6"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
